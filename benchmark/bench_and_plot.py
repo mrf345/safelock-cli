4bacf5112009d86d5b24c0234d490b523e7a90a4
Mohamed Feddad <mrf345@gmail.com>
2024-09-18 16:59:12 +0400

    chore: update readme and benchmark results

--- benchmark/bench_and_plot.py
+++ benchmark/bench_and_plot.py
@@ -5,7 +5,7 @@ import random
 import matplotlib.pyplot as plt
 from matplotlib.colors import XKCD_COLORS as plot_colors
 
-safelock_cmd = "safelock-cli"
+safelock_cmd = "~/Projects/safelock-cli/safelock-cli"
 pwd = "123456789"
 rest = "60s"
 input_path = "Videos"
@@ -13,26 +13,20 @@ output_name = "test"
 output_dir = "safelock_dump"
 runs = 3
 figure_width = 14
-figure_height = 2.5
-bar_width = 0.6
+figure_height = 2.3
+bar_width = 0.65
 measure = "Seconds"
 root = os.getcwd()
 
-def get_label(i, clean=False, key="command"):
+def get_label(i, key="command"):
     matchers = [
         ('gpg', 'gpgtar',),
         ('7z', '7zip (fastest)',),
         ('age', 'age (tar-zstd)'),
         ('safelock', 'safelock',),
     ]
-    label = next((v for m, v in matchers if m in i[key]))
 
-    if clean:
-        return label
-    if key == "label":
-        return f"{label}\n{i['size']:.2f} MB"
-
-    return f"{label}\n{i['median']:.3f}s"
+    return next((v for m, v in matchers if m in i[key]))
 
 def get_name(i):
     matchers = [
@@ -48,8 +42,8 @@ def encrypt():
     err = os.system(
         f"hyperfine --runs {runs} --prepare "
         f"'sleep {rest}' "
-        f"'echo \"{pwd}\" | {safelock_cmd} encrypt {input_path} {get_name('safelock')} --quiet' "
         f"'tar cv --zstd {input_path} | . {root}/pipe_age_password.sh | age -e -p -o {get_name('age')}' "
+        f"'echo \"{pwd}\" | {safelock_cmd} encrypt {input_path} {get_name('safelock')} --quiet' "
         f"'7z a -p{pwd} -mx1 {get_name('7z')} {input_path}' "
         f"'gpgtar -e -o {get_name('gpg')} -c --yes --batch --gpg-args \"--passphrase {pwd}\" {input_path}' "
         f"--export-json {root}/encryption.json"
@@ -62,8 +56,8 @@ def decrypt():
     err = os.system(
         f"hyperfine --runs {runs} --prepare "
         f"'rm -rf {output_dir} {output_name}_*_ && mkdir {output_dir} && sleep {rest}' "
-        f"'echo \"{pwd}\" | {safelock_cmd} decrypt {get_name('safelock')} {output_dir} --quiet' "
         f"'sleep 0.05; xdotool type \"{pwd}\"; xdotool key \"Return\" | age --decrypt {get_name('age')} | tar x --zstd -f - -C {output_dir}' "
+        f"'echo \"{pwd}\" | {safelock_cmd} decrypt {get_name('safelock')} {output_dir} --quiet' "
         f"'7z e -y -p{pwd} -mx1 {get_name('7z')} -o{output_dir}' "
         f"'gpgtar -d --yes --batch --gpg-args \"--passphrase {pwd}\" {get_name('gpg')}' "
         f"--export-json {root}/decryption.json"
@@ -73,8 +67,8 @@ def decrypt():
         exit(err)
 
 os.chdir(os.path.expanduser("~"))
-# encrypt()
-# decrypt()
+encrypt()
+decrypt()
 os.chdir(root)
 plt.margins(3.5)
 
@@ -85,16 +79,15 @@ with open("encryption.json") as f:
     data = sorted(json.load(f)['results'], key=lambda i: i['median'])
     labels = [get_label(i) for i in data]
     scores = [i['median'] for i in data]
-    colors_map = {get_label(i, 1): random.choice(list(plot_colors.values())) for i in data}
-    colors = [colors_map[get_label(i, 1)] for i in data]
+    colors_map = {get_label(i): random.choice(list(plot_colors.values())) for i in data}
+    colors = [colors_map[get_label(i)] for i in data]
 
 fig, ax = plt.subplots()
 ax.set_title('Encryption Time')
 ax.set_xlabel(measure)
-ax.yaxis.set_label_position('right')
-ax.set_ylabel('lower is better')
-ax.grid(zorder=0, axis='x', color='black')
+ax.grid(zorder=0, axis='x')
 ax.barh(labels, scores, bar_width, color=colors, zorder=3)
+ax.bar_label(ax.containers[0], label_type='edge', padding=3, fmt=lambda i: f"{i:.2f}")
 fig.set_size_inches(w=figure_width, h=figure_height)
 fig.tight_layout()
 fig.savefig("encryption-time.webp", transparent=True, format="webp")
@@ -106,15 +99,14 @@ with open("decryption.json") as f:
     data = sorted(json.load(f)['results'], key=lambda i: i['median'])
     labels = [get_label(i) for i in data]
     decryption = [i['median'] for i in data]
-    colors = [colors_map[get_label(i, 1)] for i in data]
+    colors = [colors_map[get_label(i)] for i in data]
 
 fig, ax = plt.subplots()
 ax.set_title('Decryption Time')
 ax.set_xlabel(measure)
-ax.yaxis.set_label_position('right')
-ax.set_ylabel('lower is better')
-ax.grid(zorder=0, axis='x', color='black')
+ax.grid(zorder=0, axis='x')
 ax.barh(labels, decryption, bar_width, color=colors, zorder=3)
+ax.bar_label(ax.containers[0], label_type='edge', padding=3, fmt=lambda i: f"{i:.2f}")
 fig.set_size_inches(w=figure_width, h=figure_height)
 fig.tight_layout()
 fig.savefig("decryption-time.webp", transparent=True, format="webp")
@@ -126,20 +118,19 @@ os.chdir(os.path.expanduser("~"))
 data = sorted([{
     'size': os.path.getsize(get_name(get_label(i))) / 1024 / 1024,
     'label': get_label(i),
-    'color': colors_map[get_label(i, 1)],
+    'color': colors_map[get_label(i)],
 } for i in data], key=lambda i: i['size'])
 os.chdir(root)
-labels = [get_label(i, key='label') for i in data]
+labels = [get_label(i, 'label') for i in data]
 sizes = [i['size'] for i in data]
 colors = [i['color'] for i in data]
 
 fig, ax = plt.subplots()
 ax.set_title('File Size')
-ax.set_xlabel("Megabytes")
-ax.yaxis.set_label_position('right')
-ax.set_ylabel('lower is better')
-ax.grid(zorder=0, axis='x', color='black')
+ax.set_xlabel("MegaBytes")
+ax.grid(zorder=0, axis='x')
 ax.barh(labels, sizes, bar_width, color=colors, zorder=3)
+ax.bar_label(ax.containers[0], label_type='edge', padding=3, fmt=lambda i: f"{i:.0f}")
 fig.set_size_inches(w=figure_width, h=figure_height)
 fig.tight_layout()
 fig.savefig("file-size.webp", transparent=True, format="webp")
